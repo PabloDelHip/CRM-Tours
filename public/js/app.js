4f717b7c2172e2363137634af1b374c7e23e1f22
pablodelhip <pablodelhip@gmail.com>
2021-01-24 13:45:40 -0600

    actualizando

--- public/js/app.js
+++ public/js/app.js
@@ -1994,6 +1994,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 /* harmony default export */ __webpack_exports__["default"] = ({
+  name: "login-component",
   mounted: function mounted() {
     console.log('Component mounted.');
   }
