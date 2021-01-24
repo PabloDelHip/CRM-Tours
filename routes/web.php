97ea937ed63d96553d89298671c53ac1650d40e3
pablodelhip <pablodelhip@gmail.com>
2021-01-24 13:33:31 -0600

    subiendo template y login

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', ['as' => '/', 'uses' => 'HomeController@index']);
+
+///Login///
+Route::get('login', ['as' => 'login', 'uses' => 'LoginController@index']);
