61ab3d4b5bc5b4885b46bde5a1ea8c813fbcd2fc
pablodelhip <pablodelhip@gmail.com>
2021-01-23 21:07:05 -0600

    rama-azul

---
+++ SSH.js
@@ -0,0 +1,19 @@
+Generating public/private rsa key pair.
+Enter passphrase (empty for no passphrase): 
+Enter same passphrase again: 
+Your identification has been saved in /home/ssijaeh24umj/.ssh/id_rsa.
+Your public key has been saved in /home/ssijaeh24umj/.ssh/id_rsa.pub.
+The key fingerprint is:
+d4:42:36:85:1b:b7:f6:3f:ff:d1:d4:62:4c:5c:dd:92 
+The key's randomart image is:
++--[ RSA 2048]----+
+|        +o.    .+|
+|       ooo.  .E.o|
+|        o+..  o. |
+|       ...o  o  .|
+|        S. .  + o|
+|            .. o.|
+|             . ..|
+|              o .|
+|               o+|
++-----------------+
