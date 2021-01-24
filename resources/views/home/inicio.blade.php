fbb3e9e16004f076ec06f5945a8e726960559e12
pablodelhip <pablodelhip@gmail.com>
2021-01-23 21:45:07 -0600

    subiendo vue

--- resources/views/home/inicio.blade.php
+++ resources/views/home/inicio.blade.php
@@ -5,6 +5,9 @@
 <div class="container">
     <h1>si funciona Ingresando texto</h1>
 </div>
+<div id="app" class="content"><!--La equita id debe ser app, como hemos visto en app.js-->
+    <example-component></example-component><!--Añadimos nuestro componente vuejs-->
+</div>
 
 @stop
 
