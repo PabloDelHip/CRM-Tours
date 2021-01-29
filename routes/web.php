23f3633f2973902d9fde3552d1817b1e1a68fc9c
pablodelhip <pablodelhip@gmail.com>
2021-01-29 12:01:38 -0600

    subiendo JWT

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', ['as' => '/', 'uses' => 'HomeController@index']);
 
 ///Login///
-Route::get('login', ['as' => 'login', 'uses' => 'LoginController@index']);
+Route::get('/login', ['as' => 'login', 'uses' => 'LoginController@index']);
 Route::get('/restablecer-contrasena', ['as' => '/restablecer-contrasena', 'uses' => 'LoginController@index']);
 Route::get('/nueva-contrasena', ['as' => '/nueva-contrasena', 'uses' => 'LoginController@index']);
 
