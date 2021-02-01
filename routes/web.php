a3acd9c1fc76c1a6a45df0373b6d5f64788dbace
Daniel Viveros <dviveros90@gmail.com>
2021-02-01 15:26:58 -0500

    Route was changed

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/user/login', ['as' => 'login', 'uses' => 'LoginController@index']);
 Route::get('/user/restablecer-contrasena', ['as' => '/restablecer-contrasena', 'uses' => 'LoginController@index']);
 Route::get('/user/nueva-contrasena', ['as' => '/nueva-contrasena', 'uses' => 'LoginController@index']);
 
-Route::get('/user/create', ['as' => '/create', 'uses' => 'LoginController@index']);
+Route::get('/user/create', ['as' => '/create', 'uses' => 'HomeController@index']);
 
 //Dashboard
 Route::get('/overview', ['as' => '/overview', 'uses' => 'HomeController@index']);
