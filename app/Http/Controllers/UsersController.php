5a43a2be36cc889f6e7194fbd3b459d5d944b17d
Daniel Viveros <dviveros90@gmail.com>
2021-02-01 23:29:04 -0500

    User function was added

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,10 +8,21 @@ use App\User;
 
 class UsersController extends Controller
 {
+    public function get(Request $request){
+
+    }
+
+    public function post(Request $request){
+        $user = new User();
+        $user = fill($request);
+        $request->password = Crypt::encryptString($request->password);
+        $request->save();
+        return $object;
+    }
+
     public function getCurrentUser() {
         $token = JWTAuth::getToken();
         return User::where('remember_token', $token)->get()->first();
         //return $token;
-        
     }
 }
--- resources/js/views/User/CreateUserComponent.vue
+++ resources/js/views/User/CreateUserComponent.vue
@@ -10,19 +10,19 @@
                         <div class="card-body">
                             <div class="form-group">
                                 <label for="name">Nombre</label>
-                                <input type="text" class="form-control" id="nMW" placeholder="John Doe">
+                                <input type="text" class="form-control" v-model="name"  placeholder="John Doe">
                             </div>
                             <div class="form-group">
                                 <label for="email">Correo electrónico</label>
-                                <input type="email" class="form-control" id="email" placeholder="ejemplo.fulanito@ejemplo.com">
+                                <input type="email" class="form-control" v-model="email"  placeholder="ejemplo.fulanito@ejemplo.com">
                             </div>
                             <div class="form-group">
                                 <label for="emailConfirm">Confirmar correo electrónico</label>
-                                <input type="email" class="form-control" id="emailConfirm" placeholder="ejemplo.fulanito@ejemplo.com">
+                                <input type="email" class="form-control" v-model="emailConfirm"  placeholder="ejemplo.fulanito@ejemplo.com">
                             </div>
                             <div class="form-group">
                                 <label for="password">Contraseña</label>
-                                <input type="password" class="form-control" id="password" placeholder="Contraseña">
+                                <input type="password" class="form-control" v-model="password" placeholder="Contraseña">
                             </div>
                             <div class="form-group">
                                 <label>Estatus del usuario</label>
@@ -33,7 +33,7 @@
                             </div>
                         </div>
                         <div class="card-footer">
-                            <button type="submit" class="btn btn-primary">Guardar</button>
+                            <button type="button" @click="saveUser()" class="btn btn-primary">Guardar</button>
                         </div>
                     </form>
                 </div>
@@ -41,3 +41,43 @@
         </div>
     </div>
 </template>
+
+<script>
+    import User from '../../providers/User';
+
+    const UserResource = new User();
+
+    export default{
+        name: "create-user-component",
+        data () {
+            return {
+                name: null,
+                email: null,
+                emailConfirm: null,
+                password: null,
+                message: null,
+                showError: null,
+            }
+        },
+        methods: {
+            saveUser() {
+                if (this.email != this.emailConfirm){
+                    this.message = "mal contraseña";
+                    this.showError = true;
+                }
+
+
+                let formData = {
+                    name: this.name,
+                    email: this.email,
+                    password: this.password,
+                }
+
+
+                console.log(formData)
+            }
+        },
+    }
+
+
+</script>
