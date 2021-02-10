278656806216a48e38c82c26515772fd70315184
pablodelhip <pablodelhip@gmail.com>
2021-02-09 18:22:51 -0600

    subiendo cambios

--- public/js/app.js
+++ public/js/app.js
@@ -92707,8 +92707,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\INSPIRON\laragon\www\CRM-Tours\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\INSPIRON\laragon\www\CRM-Tours\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/pablo/CRM/CRM-Tours/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/pablo/CRM/CRM-Tours/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
